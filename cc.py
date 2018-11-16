405207d9f6ac63686d32fc670064b8e69fce5518
cspidermx <cspidermx@yahoo.com.mx>
2018-11-16 13:39:10 -0600

    cc command improvements

--- cc.py
+++ cc.py
@@ -16,32 +16,38 @@ def to_gif(mp4file):
         "input": "download",
         "file": mp4file,
         "converteroptions": {
-            "command": '-i {INPUTFILE} -filter_complex "[0:v] split' 
-                        '[a][b];[a] palettegen [p];[b][p] paletteuse" {OUTPUTFILE}'
+            "command": '-i {INPUTFILE} -filter_complex '
+                       '"scale=w=\'min(240,iw)\':h=-1,split [a][b];'
+                       '[a] palettegen=stats_mode=\'diff\' [p];'
+                       '[b] fifo [c];'
+                       '[c] [p] paletteuse=diff_mode=\'rectangle\'"'
+                       ' {OUTPUTFILE}'
+            # https://engineering.giphy.com/how-to-make-gifs-with-ffmpeg/
+            # https://ffmpeg.org/ffmpeg-filters.html
         }
     })
     try:
         process.wait()
         process.download()
         fname = process.data['output']['filename']
-    except ccex.BadRequest:
-        errmsg = 'BadRequest'
-    except ccex.ConversionFailed:
-        errmsg = 'ConversionFailed'
-    except ccex.HTTPError:
-        errmsg = 'HTTPError'
-    except ccex.InvalidParameterException:
-        errmsg = 'InvalidParameterException'
-    except ccex.InvalidResponse:
-        errmsg = 'InvalidParameterException'
-    except ccex.TemporaryUnavailable:
-        errmsg = 'InvalidParameterException'
-    except ccex.APIError:
-        errmsg = 'APIError'
+    except ccex.BadRequest as e:
+        errmsg = 'BadRequest ' + e.args[0]
+    except ccex.ConversionFailed as e:
+        errmsg = 'ConversionFailed ' + e.args[0]
+    except ccex.HTTPError as e:
+        errmsg = 'HTTPError ' + e.args[0]
+    except ccex.InvalidParameterException as e:
+        errmsg = 'InvalidParameterException ' + e.args[0]
+    except ccex.InvalidResponse as e:
+        errmsg = 'InvalidParameterException ' + e.args[0]
+    except ccex.TemporaryUnavailable as e:
+        errmsg = 'InvalidParameterException ' + e.args[0]
+    except ccex.APIError as e:
+        errmsg = 'APIError ' + e.args[0]
 
     process.delete()
     if errmsg != '':
         print('Error ({}) al convertir archivo ({}) a GIF'.format(errmsg, mp4file))
-        return ''
+        return mp4file
     else:
         return fname
